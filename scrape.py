ec02d3dd2dc7e788a0c097767699ea272d60ac77
Brian <brian.salkas@gmail.com>
2023-01-24 10:42:08 -0600

    make it actually enter the answers into the browser

--- scrape.py
+++ scrape.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 driver = selenium.webdriver.Firefox()
-
 url = 'https://www.nytimes.com/puzzles/spelling-bee'
 driver.get(url)
+time.sleep(7)
 btns = driver.find_elements(By.TAG_NAME, 'button')
 found = False
 for btn in btns:
@@ -17,13 +17,20 @@ for btn in btns:
         driver.execute_script("arguments[0].click();",btn)
 if not found:
     print('unable to get todays spelling-bee, please try again')
+time.sleep(5)
 hive = driver.find_element(By.CLASS_NAME, 'hive')
 
 good_letters = []
 middle_letter = driver.find_element(By.XPATH,"//*[@class='cell-letter' or @class='center']")
 middle_letter = middle_letter.get_attribute('innerHTML')
+btns = {}
+for pol in hive.find_elements(By.TAG_NAME, 'svg'):
+    btn = pol.find_element(By.CLASS_NAME, 'cell-letter')
+    letter = btn.get_attribute('innerHTML')
+    btns[letter] = pol
 for cell in hive.find_elements(By.CLASS_NAME, 'cell-letter'):
     good_letters.append(cell.get_attribute('innerHTML'))
+
 
 bad_letters = []
 for letter in 'qwertyuiopasdfghjklzxcvbnm':
@@ -45,5 +52,18 @@ for each in words:
             break
     if is_valid and middle_letter in each:
         good_words.append(each)
-#input_field = driver.find_element(By.CLASS_NAME, 'sb-hive-input-content')
+
+# find enter button
+enter_button = driver.find_element(By.CLASS_NAME, 'hive-action__submit')
+print(enter_button, 'enter button')
+
 print(good_words)
+for word in good_words:
+    for char in word:
+        time.sleep(.5)
+        btns[char].click()    
+    print('end inner loop')
+    time.sleep(1)
+    enter_button.click()
+    driver.execute_script("arguments[0].click();", enter_button)
+
