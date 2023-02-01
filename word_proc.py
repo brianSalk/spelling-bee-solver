c77dc3df72fd8a22a5ea3f4fb4539202ea257925
Brian <brian.salkas@gmail.com>
2023-01-31 22:11:07 -0600

    add get_bad_letters to word_proc.py

--- word_proc.py
+++ word_proc.py
@@ -10,3 +10,9 @@ def get_good_words(bad_letters,words,middle_letter):
         if is_valid and middle_letter in each:
             good_words.append(each)
     return good_words
+def get_bad_letters(good_letters)
+    bad_letters = []
+    for letter in 'qwertyuiopasdfghjklzxcvbnm':
+        if letter not in good_letters:
+            bad_letters.append(letter)
+    return bad_letters
