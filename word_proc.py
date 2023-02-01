8f5f0e394670e2fb5127e37230c7bffebe6e62fb
Brian <brian.salkas@gmail.com>
2023-02-01 11:23:41 -0600

    add pydocs to functions

--- word_proc.py
+++ word_proc.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 def get_good_words(bad_letters,words,middle_letter):
+    """finds all words in the list 'words' that contain middle_letter
+    at least once and does not contain any bad letters"""
     good_words = []
     for each in words:
         each = each.lower()
@@ -13,12 +15,16 @@ def get_good_words(bad_letters,words,middle_letter):
     return good_words
 
 def get_bad_letters(good_letters):
+    """returns all the letters that are not present in the
+    spelling bee hive"""
     bad_letters = []
     for letter in 'qwertyuiopasdfghjklzxcvbnm':
         if letter not in good_letters:
             bad_letters.append(letter)
     return bad_letters
 def get_good_letters_and_buttons(driver):
+    """returns a dict of buttons mapped to each letter as well as
+    a list of all 6 of the letters that are in the hive"""
     hive = driver.find_element(By.CLASS_NAME, 'hive')
     good_letters = []
     btns = {}
@@ -30,6 +36,7 @@ def get_good_letters_and_buttons(driver):
         good_letters.append(cell.get_attribute('innerHTML'))
     return (good_letters, btns)
 def get_words():
+    """gets list of english words, exculding proper nouns"""
     with open('english_no_proper', 'r') as f:
         words = []
         for word in f:
