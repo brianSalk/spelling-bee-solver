a3b9b7a4caee349db74fe2db07c4cd6f5a3376e5
BrianSalk <brian.salkas@gmail.com>
2023-11-30 21:43:23 -0600

    Format solve.py and word_proc.py with black

--- solve.py
+++ solve.py
@@ -4,60 +4,83 @@ import sys
 import argparse
 import word_proc
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait         
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # argparse stuff
 parser = argparse.ArgumentParser()
-parser.add_argument("--browser", help="Browser used to solve spellingbee, default is firefox", default='firefox')
-parser.add_argument("-l","--login", action='store_true', help='use to manually log in to NYT')
-parser.add_argument("-v", "--verbose", action="store_true", help='print words that are being tried')
-parser.add_argument("-S","--no_solve",action="store_true", help='do not solve, only list words in terminal')
+parser.add_argument(
+    "--browser",
+    help="Browser used to solve spellingbee, default is firefox",
+    default="firefox",
+)
+parser.add_argument(
+    "-l", "--login", action="store_true", help="use to manually log in to NYT"
+)
+parser.add_argument(
+    "-v", "--verbose", action="store_true", help="print words that are being tried"
+)
+parser.add_argument(
+    "-S",
+    "--no_solve",
+    action="store_true",
+    help="do not solve, only list words in terminal",
+)
 args = parser.parse_args()
 
-# validate arguments 
-if args.browser == 'firefox':
+# validate arguments
+if args.browser == "firefox":
     driver = selenium.webdriver.Firefox()
-elif args.browser == 'chrome':
+elif args.browser == "chrome":
     driver = selenium.webdriver.Chrome()
 else:
-    print(f'unsupported browser called: {args.browser}')
-    print(f'contact me at github or open an issue to get {args.browser} support')
+    print(f"unsupported browser called: {args.browser}")
+    print(f"contact me at github or open an issue to get {args.browser} support")
     sys.exit(1)
 
-if args.login: # loggin to NYT
-    driver.get('https://myaccount.nytimes.com/auth/enter-email?redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Fpuzzles%2Fspelling-bee&amp;response_type=cookie&amp;client_id=games&amp;application=crosswords&amp;asset=navigation-bar')
+if args.login:  # loggin to NYT
+    driver.get(
+        "https://myaccount.nytimes.com/auth/enter-email?redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Fpuzzles%2Fspelling-bee&amp;response_type=cookie&amp;client_id=games&amp;application=crosswords&amp;asset=navigation-bar"
+    )
     wait = WebDriverWait(driver, 2_000)
     wait.until(EC.url_to_be("https://www.nytimes.com/?login=email&auth=login-email"))
-url = 'https://www.nytimes.com/puzzles/spelling-bee'
+url = "https://www.nytimes.com/puzzles/spelling-bee"
 driver.get(url)
-plat_btn = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[text() ='Play']")))
-driver.execute_script("arguments[0].click();",plat_btn)
-        
+plat_btn = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.XPATH, "//button[text() ='Play']"))
+)
+driver.execute_script("arguments[0].click();", plat_btn)
 
-middle_letter = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@class='cell-letter' or @class='center']")))
-middle_letter = middle_letter.get_attribute('innerHTML')
+
+middle_letter = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.XPATH, "//*[@class='cell-letter' or @class='center']")
+    )
+)
+middle_letter = middle_letter.get_attribute("innerHTML")
 
 good_letters, btns = word_proc.get_good_letters_and_buttons(driver)
 bad_letters = word_proc.get_bad_letters(good_letters)
 # get list of english dictionary words
 words = word_proc.get_words()
-good_words = word_proc.get_good_words(bad_letters, words,middle_letter)
+good_words = word_proc.get_good_words(bad_letters, words, middle_letter)
 if args.no_solve:
     print(good_words)
     sys.exit(0)
 if args.verbose:
     print(good_words)
-    
+
     """
 element = driver.find_element(By.XPATH,"//div[@class='purr-blocker-card pz-hide-games-app pz-hide-newsreader']")
 driver.execute_script("arguments[0].style.visibility='hidden'", element)
     """
 # find enter button
-enter_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'hive-action__submit')))
+enter_button = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.CLASS_NAME, "hive-action__submit"))
+)
 for word in good_words:
     for char in word:
-        btns[char].click()    
+        btns[char].click()
     time.sleep(1)
     enter_button.click()
     driver.execute_script("arguments[0].click();", enter_button)
--- word_proc.py
+++ word_proc.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-def get_good_words(bad_letters,words,middle_letter):
+
+
+def get_good_words(bad_letters, words, middle_letter):
     """finds all words in the list 'words' that contain middle_letter
     at least once and does not contain any bad letters"""
     good_words = []
@@ -16,32 +18,39 @@ def get_good_words(bad_letters,words,middle_letter):
             good_words.append(each)
     return good_words
 
+
 def get_bad_letters(good_letters):
     """returns all the letters that are not present in the
     spelling bee hive"""
     bad_letters = []
-    for letter in 'qwertyuiopasdfghjklzxcvbnm':
+    for letter in "qwertyuiopasdfghjklzxcvbnm":
         if letter not in good_letters:
             bad_letters.append(letter)
     return bad_letters
+
+
 def get_good_letters_and_buttons(driver):
     """returns a dict of buttons mapped to each letter as well as
     a list of all 6 of the letters that are in the hive"""
-    hive = WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.CLASS_NAME, 'hive'))) # driver.find_element(By.CLASS_NAME, 'hive')
+    hive = WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "hive"))
+    )  # driver.find_element(By.CLASS_NAME, 'hive')
     good_letters = []
     btns = {}
-    for pol in hive.find_elements(By.TAG_NAME, 'svg'):
-        btn = pol.find_element(By.CLASS_NAME, 'cell-letter')
-        letter = btn.get_attribute('innerHTML')
+    for pol in hive.find_elements(By.TAG_NAME, "svg"):
+        btn = pol.find_element(By.CLASS_NAME, "cell-letter")
+        letter = btn.get_attribute("innerHTML")
         btns[letter] = pol
-    for cell in hive.find_elements(By.CLASS_NAME, 'cell-letter'):
-        good_letters.append(cell.get_attribute('innerHTML'))
+    for cell in hive.find_elements(By.CLASS_NAME, "cell-letter"):
+        good_letters.append(cell.get_attribute("innerHTML"))
     return (good_letters, btns)
+
+
 def get_words():
     """gets list of english words, exculding proper nouns"""
-    with open('english_no_proper', 'r') as f:
+    with open("english_no_proper", "r") as f:
         words = []
         for word in f:
-            word = word.replace('\n','')
+            word = word.replace("\n", "")
             words.append(word)
     return words
